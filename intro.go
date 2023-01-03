ab72a133c1aa67146a5e095a781cee82b70e5613
Code Rio <kayodeanointed@gmail.com>
2023-01-03 11:43:47 +0100

    jan 3

--- intro.go
+++ intro.go
@@ -2,11 +2,45 @@ package main
 
 import "fmt"
 
-func main(){
+// You can define a variable outside the function main
+// don't do these
+// 	var d string = "cat"
+// 	var e string = "food"
+// 	var f int = 34
+// // do these instead 
+// 	var {
+// 		g int = 34
+// 		h string = "food"
+// 		j string = "cat"
+// 	}
+// 	var {
+// 		counter int = 0
+// 	}
+// 	fmt.Printf(d)
+// 	fmt.Printf(e)
+// 	fmt.Printf(f)
+// 	fmt.Printf(g)
+// 	fmt.Println(h)
+// 	fmt.Println(j)
 
+func main(){
 	fmt.Printf("Hello world")
-	fmt.Printf("Coding is Beautiful")
-	fmt.Printf("Code Rio")
-	fmt.Printf("Coderio97")
-	fmt.Printf("Coderio10")
+// Variables
+// 1st and 2nd method, you get to define the type of variable. The 3rd method, computer detects the type of variable 
+	// 1st method
+		var a int
+		a = 27 
+		fmt.Println(a)
+	// 2nd method
+		// integer
+			var b int = 42
+			fmt.Println(b)
+		// float
+			var b1 float32 = 25.5
+			fmt.Printf("%v, %T\n", b1, b1) 
+	// 3rd Method
+		i := 42.
+		fmt.Println(i)
+		// detect variable
+		fmt.Printf("%v, %T", i, i)
 }
